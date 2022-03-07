7c28958df81b758dd8d5630644d8e666feeb55de
Thekasperdahl <Kasper_dahl_pedersen@hotmail.com>
2022-03-07 17:20:52 +0100

    FUCK DET VIRKER!

--- kalenderjava.js
+++ kalenderjava.js
@@ -21,11 +21,11 @@ window.addEventListener("scroll", parallax, false);
   //her kan man edit i css så man kan vise forskellige dele i HTMLen
 
  //function loopuge(x) {
-	let uge1=document.getElementById("uge1")
-	let uge2=document.getElementById("uge2")
-	let uge3=document.getElementById("uge3")
+//	let uge1=document.getElementById("uge1")
+//	let uge2=document.getElementById("uge2")
+//	let uge3=document.getElementById("uge3")
 
-	const uger=["uge1", "uge2", "uge3"]
+//	const uger=["uge1", "uge2", "uge3"]
 	//if (x==1) {u++}
 
 	//for(let i = 0; i < uger.length;i++) {
@@ -34,8 +34,32 @@ window.addEventListener("scroll", parallax, false);
 	//}
   //}
 
-function test(){
-	elem.parentElement
-		if(uger==0){elem.parentElement.style.display = "block"}  
+//function test(){
+//	elem.parentElement
+//		if(uger==0){elem.parentElement.style.display = "block"}  
+//}
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("uge");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
