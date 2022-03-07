14c22bd6ef507d0e43b28c3d4ebf82a9d83d5e94
Artaey <95622783+Artaey@users.noreply.github.com>
2022-03-07 09:27:04 +0100

    Log in thing WIP
    WIP

---
+++ project4.js
@@ -0,0 +1,66 @@
+// Start Lynge's login 
+let checks = [false, false];
+
+function bvalid(){
+    let text = document.getElementById("berr");
+    if(document.getElementById("bind").value){
+        text.innerHTML = "Tjek";
+        text.style.color = "green";
+        checks[0] = true;
+    }else{
+        text.innerHTML = "Skriv brugernevn";
+        text.style.color = "red";
+        checks[0] = false;
+    }
+}
+
+function kvalid(){
+    let pass = document.getElementById("kind");
+    if(pass.value){
+        let text = document.getElementById("kerr");
+        if(pass.value.length > 7){
+            text.innerHTML = "Tjek";
+            text.style.color = "green";
+            checks[1] = true;
+        }else{
+            text.innerHTML = "Kodeord for kort, minimum 8 tegn";
+            text.style.color = "red";
+            checks[1] = false;
+        }
+        
+    }else{
+        let text = document.getElementById("kerr")
+        text.innerHTML = "Skriv kodeord";
+        text.style.color = "red";
+        checks[1] = false;
+    }
+}
+
+function seep(){
+    let pass = document.getElementById("kind");
+    let btn = document.getElementById("btnveiw");
+    if(pass.type == "password"){
+        pass.type = "text";
+        btn.innerHTML = "skjul"
+    }else{
+        pass.type = "password";
+        btn.innerHTML = "see"
+    }
+    
+}
+
+function sub(){
+    let checker = 0;
+    for(let i = 0; i < checks.length; i++){
+        if(checks[i] == true){
+            checker++;
+        }
+    }
+    if(checker == checks.length){
+        alert("Ind");
+    }else{
+        alert("Brugernavn eller kodeord er forkert")
+    }
+}
+
+// Slut Lynge's login
