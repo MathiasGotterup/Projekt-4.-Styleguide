d54b7b66b35783e94b65fcc25f04798356f2e1fb
Thekasperdahl <95621877+Thekasperdahl@users.noreply.github.com>
2022-03-08 12:54:56 +0100

    java done

--- kalenderjava.js
+++ kalenderjava.js
@@ -1,41 +1,21 @@
 function parallax(){
-	var prlx_lyr_1 = document.getElementById('prlx_lyr_1');
-	var prlx_lyr_2 = document.getElementById('prlx_lyr_2');
+	let prlx_lyr_1 = document.getElementById('prlx_lyr_1');
+	let prlx_lyr_2 = document.getElementById('prlx_lyr_2');
 	prlx_lyr_1.style.top = -(window.pageYOffset / 4)+'px';
 	prlx_lyr_2.style.top = -(window.pageYOffset / 8)+'px';
 }
 window.addEventListener("scroll", parallax, false);
 
-//function loopuge(){
-	//document.getElementById(uge2).style.display = "block"
-//}
 
-//function ifelse() {
-	//let x = document.getElementById("");
-	//if (x.style.display === "none") {
-	  //x.style.display = "block";
-	//} else {
-	  //x.style.display = "none";
-	//}
-  //}
-  //her kan man edit i css så man kan vise forskellige dele i HTMLen
-
- //function loopuge(x) {
-//	let uge1=document.getElementById("uge1")
-//	let uge2=document.getElementById("uge2")
-//	let uge3=document.getElementById("uge3")
-
-//	const uger=["uge1", "uge2", "uge3"]
-	//if (x==1) {u++}
-
-	//for(let i = 0; i < uger.length;i++) {
-	//if (i==u) {uger[i].style.display=block}
-	//else {uger[i].style.display=none}
-	//}
-  //}
-
-
-var slideIndex = 1;
+ function loopuge(x) {
+	let uge1=document.getElementById("uge1");
+	let uge2=document.getElementById("uge2");
+	let uge3=document.getElementById("uge3");
+ }
+	const uger=["uge1", "uge2", "uge3"];
+	console.log(uger);
+	
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
